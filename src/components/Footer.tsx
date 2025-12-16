8724cf2bd67dc9fb5bb256c170f42c7648c80353
Harshdeep Singh <94488766+harshdeepsinghin@users.noreply.github.com>
2025-12-16 23:54:21 +0530

    refactor: add social links (YouTube Channel Link) in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,14 +16,16 @@ export default function Footer() {
             <li><Link href="/contact">Contact</Link></li>
           </ul>
         </div>
-        {/* <div className="footer-section">
-          <h4>Resources</h4>
-          <ul>
-            <li><Link href="#">Documentation</Link></li>
-            <li><Link href="#">Tutorials</Link></li>
-            <li><Link href="#">Support</Link></li>
-          </ul>
-        </div> */}
+        <div className="footer-section text-center md:text-left">
+          <h4>Follow Us</h4>
+          <div className="social-links flex justify-center md:justify-start items-center">
+            <a href="https://www.youtube.com/@LinuxWaleOfficial" target="_blank" rel="noopener noreferrer" aria-label="YouTube">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
+              </svg>
+            </a>
+          </div>
+        </div>
       </div>
       <div className="footer-bottom">
         <p>&copy; 2025 LinuxWale. Embracing the power of FOSS.</p>
